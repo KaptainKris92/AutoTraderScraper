32f270b266de9d98341111f6d79a864d286f8ce5
KaptainKris92 <krisadamatzky@yahoo.co.uk>
2025-07-25 22:27:11 +0100

    Add argparse argument for --scroll-until-end when scraping

--- scraper.py
+++ scraper.py
@@ -407,13 +407,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--scrape", action="store_true", help="Scrape new ads from AutoTrader")
     parser.add_argument("--download", action="store_true", help="Download images for saved ads")
-    parser.add_argument("--limit", type=int, default=None, help="Limit number of ads to download")
-    parser.add_argument("--max-scrolls", type=int, default = DEFAULT_MAX_SCROLLS, help = "How many times to scroll during scraping.")
+    parser.add_argument("--limit", type=int, default=None, help="Limit number of missing images to download (generally just used for debugging)")
+    parser.add_argument("--max-scrolls", type=int, default = DEFAULT_MAX_SCROLLS, help = "How many times to scroll during scraping. (Alternatively, use `--scroll-until-end`)")
+    parser.add_argument("--scroll-until-end", action="store_true", help="Keep scrolling until all ads are loaded.")
     
     args = parser.parse_args()
 
     if args.scrape:
         create_ads_table()
+        max_scrolls = 9999 if args.scroll_until_end else args.max_scrolls # 9999 should be sufficient.
         df = scrape_autotrader(max_scrolls = args.max_scrolls)
         save_to_sql(df, TABLE_NAME)
     if args.download:
